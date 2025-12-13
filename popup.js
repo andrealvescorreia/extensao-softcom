541a45ad06793c92322d20e0290fe2362ce8acad
André Alves Correia <andre2013aguabranca@gmail.com>
2025-12-13 05:23:10 -0300

    refactor

--- popup.js
+++ popup.js
@@ -1,20 +1,22 @@
 const searchButton = document.getElementById("searchButton");
 const searchButtonFullName = document.getElementById("searchButtonFullName");
+const buttonSearchByCode = document.getElementById("buttonSearchByCode");
 const sped = document.getElementById("sped");
 const apoio = document.getElementById("apoio");
+const useCapturedClient = document.getElementById("useCapturedClient");
 
-function searchNameOnAreaPartner(clientName) {
+function searchClientByNameOnAreaPartner(clientName) {
   const url = `https://areapartner.softcomsistemas.com.br/cliente/index?&nome_cliente=${clientName}`;
 
   chrome.tabs.create({ url: url });
 }
 
-function handleSearch(action) {
+function handleAction(action, supportedUrl) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (!tabs[0]) return;
-    // Verificar se está na página de chat
-    if (!tabs[0].url.includes("chat.clientesatisfeito.com.br")) {
-      alert("Disponivel apenas na pagina chat.clientesatisfeito.com.br");
+    // Verifica se está na página correta para a ação
+    if (!tabs[0].url.includes(supportedUrl)) {
+      alert("Disponivel apenas na pagina " + supportedUrl);
       return;
     }
 
@@ -25,57 +27,34 @@ function handleSearch(action) {
         alert("Erro ao enviar mensagem. Tente recarregar a pagina.");
         return;
       }
-      searchNameOnAreaPartner(response.clientName);
+      if (
+        action === "searchButtonClicked" ||
+        action === "searchButtonFullNameClicked"
+      ) {
+        searchClientByNameOnAreaPartner(response.clientName);
+      }
     });
   });
 }
 
 searchButton.addEventListener("click", () => {
-  handleSearch("searchButtonClicked");
+  handleAction("searchButtonClicked", "chat.clientesatisfeito.com.br");
 });
 
 searchButtonFullName.addEventListener("click", () => {
-  handleSearch("searchButtonFullNameClicked");
+  handleAction("searchButtonFullNameClicked", "chat.clientesatisfeito.com.br");
 });
 
 sped.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (!tabs[0]) return;
-    // Verificar se está na página da area partner
-    if (!tabs[0].url.includes("areapartner.softcomsistemas.com.br/")) {
-      alert("Disponivel apenas na pagina areapartner.softcomsistemas.com.br/");
-      return;
-    }
-
-    // Enviar mensagem ao content script da aba ativa
-    chrome.tabs.sendMessage(tabs[0].id, { action: "sped" }, (response) => {
-      if (chrome.runtime.lastError) {
-        console.log("Erro ao enviar mensagem:", chrome.runtime.lastError);
-        alert("Erro ao enviar mensagem. Tente recarregar a pagina.");
-        return;
-      }
-      console.log(response);
-    });
+    handleAction("sped", "areapartner.softcomsistemas.com.br");
   });
 });
 
 apoio.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (!tabs[0]) return;
-    // Verificar se está na página da area partner
-    if (!tabs[0].url.includes("areapartner.softcomsistemas.com.br/")) {
-      alert("Disponivel apenas na pagina areapartner.softcomsistemas.com.br/");
-      return;
-    }
-
-    // Enviar mensagem ao content script da aba ativa
-    chrome.tabs.sendMessage(tabs[0].id, { action: "apoio" }, (response) => {
-      if (chrome.runtime.lastError) {
-        console.log("Erro ao enviar mensagem:", chrome.runtime.lastError);
-        alert("Erro ao enviar mensagem. Tente recarregar a pagina.");
-        return;
-      }
-      console.log(response);
-    });
+    handleAction("apoio", "areapartner.softcomsistemas.com.br");
   });
 });
+
+useCapturedClient.addEventListener("change", () => {});
