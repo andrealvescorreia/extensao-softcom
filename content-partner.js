294efa707328cc9f79b77cccac301df23ce0ebb4
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2025-12-30 13:57:28 -0300

    Add custom styles for panel and page content
    Introduced styles for .panel-body.fixed-height to increase height and max-height, and adjusted padding for .page-content in body.inspinia.

--- content-partner.js
+++ content-partner.js
@@ -2,7 +2,14 @@
 const style = document.createElement('style');
 style.textContent = `
     .col-md-6 {
-        width: 100% !important;
+        width: 100% !important; 
+    }
+    .panel-body.fixed-height {
+        height: 200px !important;
+        max-height: 1520px !important;
+    }
+    body.inspinia  .page-content {
+        padding: 0 30px 34px !important;
     }
 `;
 document.head.appendChild(style);
